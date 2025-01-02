42d8abb721893c46c3a4fb8c9d1c7b0f331ebb51
6ill <kyrbill123@gmail.com>
2025-01-02 13:35:00 +0700

    docs/install and setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   app.useGlobalPipes(new ValidationPipe({transform: true}))
   app.setGlobalPrefix('/v1/api')
+
+  const config = new DocumentBuilder()
+    .setTitle('E-Commerce API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/v1/api/docs', app, documentFactory);
+
   await app.listen(configService.get('NESTJS_PORT'));
 }
 bootstrap();
